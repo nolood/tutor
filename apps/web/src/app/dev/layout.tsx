d260d1bca8d234ea6d14f7bbc6af6805eeaac2bf
f1k13 <you@example.com>
2024-09-11 20:06:24 +0300

    fixed: delete checked ci on env

--- apps/web/src/app/dev/layout.tsx
+++ apps/web/src/app/dev/layout.tsx
@@ -1,12 +1,6 @@
-import { notFound } from "next/navigation";
 import { ReactNode } from "react";
-import { env } from "~/env";
 
 const Layout = ({ children }: { children: ReactNode }) => {
-  if (env.NODE_ENV !== "development") {
-    return notFound();
-  }
-
   return <div>{children}</div>;
 };
 
